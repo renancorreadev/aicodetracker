f52b4fdb4ad1896902bf52288aa21df0a672e103
Renan Correa <renancorreadev@gmail.com>
2026-03-16 22:22:14 -0300

    feat: register toggle command

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,15 @@
 import * as vscode from 'vscode';
 
+let isEnabled = true;
+
 export function activate(context: vscode.ExtensionContext) {
-  console.log('AICodeTracker is now active');
+  isEnabled = vscode.workspace.getConfiguration('aicodetracker').get('enabled', true);
+
+  const toggleCmd = vscode.commands.registerCommand('aicodetracker.toggle', () => {
+    isEnabled = !isEnabled;
+  });
+
+  context.subscriptions.push(toggleCmd);
 }
 
 export function deactivate() {}
