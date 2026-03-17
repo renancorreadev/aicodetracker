2c30e662961e15eb1116632d3c4dc75b6a58f2c9
Renan Correa <renancorreadev@gmail.com>
2026-03-16 22:22:15 -0300

    feat: add status bar item with toggle state

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,27 @@
 import * as vscode from 'vscode';
 
 let isEnabled = true;
+let statusBarItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
   isEnabled = vscode.workspace.getConfiguration('aicodetracker').get('enabled', true);
 
+  statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+  statusBarItem.command = 'aicodetracker.toggle';
+  updateStatusBar();
+  statusBarItem.show();
+
   const toggleCmd = vscode.commands.registerCommand('aicodetracker.toggle', () => {
     isEnabled = !isEnabled;
+    updateStatusBar();
   });
 
-  context.subscriptions.push(toggleCmd);
+  context.subscriptions.push(toggleCmd, statusBarItem);
+}
+
+function updateStatusBar() {
+  statusBarItem.text = isEnabled ? '$(eye) AICodeTracker: ON' : '$(eye-closed) AICodeTracker: OFF';
+  statusBarItem.tooltip = 'Click to toggle AICodeTracker highlights';
 }
 
 export function deactivate() {}
