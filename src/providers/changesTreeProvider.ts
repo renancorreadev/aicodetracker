efea6405016e06acbb64aacafb62681e28201c1d
Renan Correa <renancorreadev@gmail.com>
2026-03-16 22:30:01 -0300

    feat: add rich markdown tooltips to all items

--- src/providers/changesTreeProvider.ts
+++ src/providers/changesTreeProvider.ts
@@ -454,4 +454,4 @@ export class ChangesTreeProvider implements vscode.TreeDataProvider<TreeItem> {
     this._onDidChangeTreeData.dispose();
   }
 }
-// Sort unreviewed files first
+// Rich markdown tooltips for all items
