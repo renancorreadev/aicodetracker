27005d6906d8bee7d9363b5dfb14fc4476796f4a
Renan Correa <renancorreadev@gmail.com>
2026-03-16 22:30:11 -0300

    feat: scaffold ClipboardReviewProvider

---
+++ src/providers/clipboardReviewProvider.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import { GitDiffProvider } from './gitDiffProvider';
+import { ChangedFile, FileStatus } from '../types';
+
+export class ClipboardReviewProvider {
+  constructor(private gitDiffProvider: GitDiffProvider) {}
+}
